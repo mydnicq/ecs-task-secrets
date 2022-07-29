995d19ab8b5318ac40ee0b0f2c9c863ec1fdaf36
Tadej Stanic <stanic.tadej@gmail.com>
2022-07-29 11:42:35 +0200

    add GA workflows

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use aws_sdk_ecs::model::Secret as AwsSecret;
 use aws_sdk_ecs::Error;
 use aws_types::{region::Region, SdkConfig};
-use aws_sdk_ecs::model::Secret as AwsSecret;
-use serde::Serialize;
-use std::fs::{File, remove_file};
 use clap::Parser;
+use serde::Serialize;
+use std::fs::{remove_file, File};
 use std::io::Write;
 use std::path::Path;
 
 const SECRETS_FILE_NAME: &str = "secrets.json";
 
+#[allow(non_snake_case)]
 #[derive(Debug, Serialize)]
 struct Secret {
     pub name: String,
@@ -27,14 +28,14 @@ impl Secrets {
     }
 }
 
-impl <'a> FromIterator<&'a AwsSecret> for Secrets {
-    fn from_iter<I: IntoIterator<Item=&'a AwsSecret>>(iter: I) -> Self {
+impl<'a> FromIterator<&'a AwsSecret> for Secrets {
+    fn from_iter<I: IntoIterator<Item = &'a AwsSecret>>(iter: I) -> Self {
         let mut secrets = Secrets::new();
-        
-        for s in iter{
+
+        for s in iter {
             secrets.add(Secret {
                 name: s.name.to_owned().unwrap(),
-                valueFrom: s.value_from.to_owned().unwrap()
+                valueFrom: s.value_from.to_owned().unwrap(),
             })
         }
 
@@ -42,27 +43,36 @@ impl <'a> FromIterator<&'a AwsSecret> for Secrets {
     }
 }
 
-struct GetTaskSecretParams <'a> {
+struct GetTaskSecretParams<'a> {
     config: &'a SdkConfig,
-    familiy: String
+    familiy: String,
 }
-
 
 async fn get_task_secrets(params: GetTaskSecretParams<'_>) -> Result<(), aws_sdk_ecs::Error> {
     let client = aws_sdk_ecs::Client::new(params.config);
 
-    let td_output = client.describe_task_definition().task_definition(params.familiy).send().await?;
+    let td_output = client
+        .describe_task_definition()
+        .task_definition(params.familiy)
+        .send()
+        .await?;
 
     let task_definition = td_output.task_definition.unwrap();
     let container_definitions = task_definition.container_definitions().unwrap();
 
-    let secrets: Secrets = container_definitions.first().unwrap().secrets().unwrap().into_iter().collect();
+    let secrets: Secrets = container_definitions
+        .first()
+        .unwrap()
+        .secrets()
+        .unwrap()
+        .iter()
+        .collect();
     let secrets_json = serde_json::to_string(&secrets).unwrap();
 
     let mut output = File::create(SECRETS_FILE_NAME).unwrap();
     write!(output, "{}", secrets_json).unwrap();
 
-    println!("{}", "Json File with Task Definition secrets created.");
+    println!("Json File with Task Definition secrets created.");
 
     Ok(())
 }
@@ -85,28 +95,29 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-
     let args = Args::parse();
 
-    let region = args.aws_region_name.unwrap_or("us-east-1".into());
+    let region = args.aws_region_name.unwrap_or_else(|| "us-east-1".into());
 
     let config = aws_config::from_env()
-    .credentials_provider(
-        aws_config::profile::ProfileFileCredentialsProvider::builder()
-        .profile_name(args.aws_profile_name)
-        .build()
-    ).region(Region::new(region))
-    .load()
-    .await;
+        .credentials_provider(
+            aws_config::profile::ProfileFileCredentialsProvider::builder()
+                .profile_name(args.aws_profile_name)
+                .build(),
+        )
+        .region(Region::new(region))
+        .load()
+        .await;
 
     if Path::new(SECRETS_FILE_NAME).exists() {
-        remove_file(SECRETS_FILE_NAME).expect(format!("Couldn't delete {}", SECRETS_FILE_NAME).as_str());
+        remove_file(SECRETS_FILE_NAME)
+            .unwrap_or_else(|_| panic!("Couldn't delete {}", SECRETS_FILE_NAME));
     }
 
     let params = GetTaskSecretParams {
-        config : &config,
-        familiy: args.family
+        config: &config,
+        familiy: args.family,
     };
-    
+
     get_task_secrets(params).await
 }
